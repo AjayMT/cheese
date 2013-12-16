5fe5d6cc9ad189d63f2e6ec6c27e1bd9ee208af7
AjayMT <ajay.tatachar@gmail.com>
2013-12-16 16:51:23 +0530

    More additions to the DOM stuff.

--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,24 @@
 var DOMUtils = {};
 
 (function ($) {
-  function listContains (list, element, compareFunc) {
+  function listContains (list, element, compareFunc, context) {
     for (var i = 0; i < list.length; i++)
-      if (! compareFunc(element, list[i])) return false;
+      if (! compareFunc.call(context, element, list[i])) return false;
+    
+    return true;
+  }
+  
+  function listsMatch (left, right, compareFunc, context) {
+    if (left.length !== right.length) return false;
+    for (var i = 0; i < left.length; i++)
+      if (! compareFunc.call(context, left[i], right[i])) return false;
     
     return true;
   }
   
   DOMUtils.insertDOMElementAtIndex =  function (parent, index, element) {
-    if (! parent.childNodes[index + 1]) parent.appendChild(element);
-    else parent.insertBefore(element, parent.childNodes[index + 1]);
+    if (! parent.children[index + 1]) parent.appendChild(element);
+    else parent.insertBefore(element, parent.children[index + 1]);
     
     return element;
   };
@@ -18,13 +26,12 @@ var DOMUtils = {};
   DOMUtils.findAbsoluteChildren = function (element) {
     var absoluteChildren = [];
     
-    for (var i = 0; i < element.childNodes.length; i++) {
-      var child = element.childNodes[i];
-      if (child.childNodes.length > 0) {
+    for (var i = 0; i < element.children.length; i++) {
+      var child = element.children[i];
+      if (child.children.length > 0)
         absoluteChildren = absoluteChildren.concat(this.findAbsoluteChildren(child));
-      } else {
+      else
         absoluteChildren.push(child);
-      }
     }
     
     return absoluteChildren;
@@ -32,68 +39,67 @@ var DOMUtils = {};
   
   DOMUtils.findParents = function (element) {
     var parents = [];
-    if (element.parentNode)
-      if (element.parentNode !== document) {
-        parents = parents.concat(element.parentNode);
-        parents = parents.concat(this.findParents(element.parentNode));
-      }
+    
+    if (! element) return [];
+    
+    if (element.parentElement) {
+      parents.push(element.parentElement);
+      parents = parents.concat(this.findParents(element.parentElement));
+    }
     
     return parents;
   };
   
-  DOMUtils.tagsMatch = function (left, right) {
-    for (var k in $(left).attr(left.attributes))
-      if (! $(right).attr(k)) return false;
-
-    for (var j in $(right).attr(right.attributes))
-      if (! $(left).attr(j)) return false;
+  DOMUtils.getAttributeNamesForElement = function (element) {
+    var names = [];
     
-    return (left.tagName === right.tagName);
+    if (! element) return [];
+    
+    for (var i = 0; i < element.attributes.length; i++)
+      names.push(element.attributes[i].name);
+    
+    return names;
+  };
+  
+  DOMUtils.tagsMatch = function (left, right) {
+    var leftAttr = this.getAttributeNamesForElement(left);
+    var rightAttr = this.getAttributeNamesForElement(right);
+    
+    if (! left || ! right) return false;
+    
+    return (left.tagName === right.tagName && listsMatch(leftAttr, rightAttr, 
+                                                         function (a, b) { return a === b; }, window));
   };
   
   DOMUtils.diffTagLists = function (left, right) {
-    var diff = { added: {}, removed: {} };
-    
+    var diff = { added: [], removed: [] };
     for (var i = 0; i < right.length; i++)
-      if (! listContains(left, right[i], this.tagsMatch)) diff.added[i] = right[i];
+      if (! listContains(left, right[i], this.tagsMatch, this)) diff.added.push(right[i]);
     
     for (var j = 0; j < left.length; j++)
-      if (! listContains(right, left[i], this.tagsMatch)) diff.remove[i] = left[i];
+      if (! listContains(right, left[i], this.tagsMatch, this)) diff.removed.push(left[i]);
     
     return diff;
   };
   
-  DOMUtils.constructChildParentMap = function (element) {
-    var absoluteChildren = this.findAbsoluteChildren(element);
-    var map = {};
-    
-    for (var i = 0; i < absoluteChildren.length; i++) {
-      map[absoluteChildren[i]] = this.findParents(absoluteChildren[i]);
-    }
-    
-    return map;
-  };
-  
   DOMUtils.getChildrenWithCommonAncestry = function (left, right) {
-    var common = {};
+    var common = [];
     
-    for (var leftChild in left)
-      for (var rightChild in right)
-        if (this.diffTagLists(left[leftChild], right[rightChild]).added === {} &&
-            this.diffTagLists(left[leftChild], right[rightChild]).removed === {})
-          if (Object.keys(common).indexOf(left[leftChild]) === -1) common[left[leftChild]] = rightChild;
+    for (var i = 0; i < left.length; i++)
+      for (var j = 0; j < right.length; j++)
+        if (listsMatch(this.findParents(left[i]), this.findParents(right[j]), this.tagsMatch, this))
+          if (common.indexOf(right[j]) === -1) common.push([left[i], right[j]]);
     
     return common;
   };
   
   DOMUtils.updateDOMElement = function (left, right) {
-    var leftMap = this.constructChildParentMap(left);
-    var rightMap = this.constructChildParentMap(right);
-    var commonMap = this.getChildrenWithCommonAncestry(leftMap, rightMap);
+    var leftChildren = this.findAbsoluteChildren(left);
+    var rightChildren = this.findAbsoluteChildren(right);
+    var commonChildren = this.getChildrenWithCommonAncestry(leftChildren, rightChildren);
     
-    for (var list in commonMap) {
-      var old = Object.keys(leftMap)[Object.keys(leftMap).indexOf(commonMap[list])];
-      $(old).replaceWith(commonMap[list]);
+    for (var i = 0; i < commonChildren.length; i++) {
+      $(commonChildren[i][0]).replaceWith(commonChildren[i][1]);
     }
   };
 })(jQuery);
