b4a48c6ae2e82a0fe8258f75894916a09a24d67f
AjayMT <ajay.tatachar@gmail.com>
2014-01-30 20:08:21 +0530

    Basic messaging API on the server.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,25 @@ var dbFile;
 
 var Cheese = {
   db: {},
-  staticData: {}
+  staticData: {},
+  messageHandlers: {}
 };
 
 Cheese.response = function (url, callback) {
   this.staticData[url] = callback;
 };
+
+Cheese.connect = function (f) {
+  this.connectHandler = f;
+}
+
+Cheese.disconnect = function (f) {
+  this.disconnectHandler = f;
+}
+
+Cheese.on = function (msg, f) {
+  this.messageHandlers[msg] = f;
+}
 
 Object.defineProperty(Cheese, 'dbFile', {
   get: function () {
@@ -17,7 +30,7 @@ Object.defineProperty(Cheese, 'dbFile', {
   },
   set: function (newPath) {
     dbFile = path.resolve(newPath);
-    
+
     if (fs.existsSync(dbFile))
       Cheese.db = JSON.parse(fs.readFileSync(dbFile, { encoding: 'utf-8' }));
     else
--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ var func = function (port, clientData, staticData, mainFilePath) {
   if (mainFilePath) Cheese = require(path.resolve(mainFilePath));
   if (Cheese.dbFile)
     fs.writeFileSync(Cheese.dbFile, JSON.stringify(Cheese.db));
-  
+
   var server = http.createServer(function (req, res) {
     if (req.url === '/__client/client.js') {
       res.writeHead(200, { 'content-type': 'application/javascript' });
@@ -32,7 +32,7 @@ var func = function (port, clientData, staticData, mainFilePath) {
       var jQueryScript = fs.readFileSync(path.join(__dirname, 'jquery-1.10.2.min.js'), { 'encoding': 'utf-8' }) + '\n';
       res.end(jQueryScript + diffScript + DOMScript + body);
       return;
-    }  else if (req.url.indexOf('/__static') !== -1) {
+    } else if (req.url.indexOf('/__static') !== -1) {
       res.writeHead(200);
       var requestPath = req.url.split('/').splice(2).join('/');
       var content;
@@ -41,41 +41,59 @@ var func = function (port, clientData, staticData, mainFilePath) {
       res.end(content);
       return;
     }
-    
+
     res.writeHead(200, { 'content-type': 'text/html' });
     var html = fs.readFileSync(path.join(__dirname, 'index.html'), { 'encoding': 'utf-8' });
     res.end(html);
   });
-  
+
   server.listen(port);
   io = io.listen(server);
-  
+
   configureSocketIO();
-  
+
   var clients = [];
-  
+  var sock = {};
+
   io.sockets.on('connection', function (socket) {
     var index = clients.length;
     clients.push(diffUtils.copyObject(Cheese.db));
-    
+
     function updateServer () {
       socket.broadcast.emit('msg', diffUtils.createDiff(Cheese.db, clients[index]));
       Cheese.db = diffUtils.copyObject(clients[index]);
-      
+
       if (Cheese.dbFile) fs.writeFileSync(Cheese.dbFile, JSON.stringify(Cheese.db));
-      
+
       for (var i = 0; i < clients.length; i++)
         clients[i] = diffUtils.copyObject(Cheese.db);
     }
-    
+
+    sock.update = updateServer;
+    sock.emit = function (m, d) {
+      socket.emit('custom', { msg: m, args: d });
+    };
+    sock.emitOthers = function (m, d) {
+      socket.broadcast.emit('custom', { msg: m, args: d });
+    };
+    sock.emitAll = function (m, d) {
+      io.sockets.emit('custom', { msg: m, args: d });
+    };
+
+    if (Cheese.connectHandler) Cheese.connectHandler(sock);
     socket.emit('init', clients[index]);
-    
+
+    socket.on('custom', function (msg) {
+      if (Cheese.messageHandlers[msg.m]) Cheese.messageHandlers(msg.args, sock, clients[index]);
+    });
+
     socket.on('msg', function (diff) {
       diffUtils.applyDiff(diff, clients[index]);
       updateServer();
     });
-    
+
     socket.on('disconnect', function () {
+      if (Cheese.disconnectHandler) Cheese.disconnectHandler(clients[index]);
       clients.splice(index, 1);
     });
   });
