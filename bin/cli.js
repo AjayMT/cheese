8faca85ab365c08468a2f668dabe54ffb8ee594d
Ajay MT <ajay.tatachar@gmail.com>
2014-10-08 18:06:37 +0530

    Only reload server when files listed in cheese.json change

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 /* global require, process, module */
 
+// require()s
 var path = require('path');
 var fs = require('fs');
 
@@ -34,18 +35,24 @@ if (opt.argv.help) {
 
 var port = opt.argv.port || 3000;
 
+// startServer: start a server in directory 'dirname', listening
+// on port 'port'. 'reload' is an optional argument used when
+// the server is reloaded.
 function startServer (dirname, port, reload) {
   debug('starting server on port ' + port);
 
   if (! reload) reload = false;
 
-  var jsonString = fs.readFileSync(path.join(dirname, 'cheese.json'), { 'encoding': 'utf-8' });
+  var jsonString = fs.readFileSync(path.join(dirname, 'cheese.json'),
+                                   { encoding: 'utf-8' });
   var mainFilePath = JSON.parse(jsonString).main;
   var clientFiles = JSON.parse(jsonString).client;
   var clientPaths = [];
   var clientData = '';
   var staticData = {};
 
+  // traverseDir: traverse a directory tree and return a list
+  // of files.
   var traverseDir = function (d) {
     var files = fs.readdirSync(d);
     var finalList = [];
@@ -65,23 +72,41 @@ function startServer (dirname, port, reload) {
 
   clientPaths = _.map(clientPaths, function (p) { return path.join('.', p); });
   _.each(clientPaths, function (elem, index, list) {
+    // we assume that file names ending with 'js' aren't static content
     if (_.last(elem.split('.')) === 'js')
       clientData += '\n' + fs.readFileSync(elem, { 'encoding': 'utf-8' });
-    else {
+    else
       staticData[elem] = fs.readFileSync(elem, { 'encoding': 'utf-8' });
-    }
   });
 
   if (! reload) server.start(port, clientData, staticData, mainFilePath);
   else server.reload(clientData, staticData, mainFilePath);
 }
 
+// watchFiles: set up a file watcher to reload the server every time
+// a file in 'dirname' changes.
 function watchFiles (dirname) {
   var watcher = chokidar.watch(path.resolve(dirname), { ignoreInitial: true });
 
-  watcher.on('all', function () {
-    debug('reloading server');
-    startServer(dirname, port, true);
+  watcher.on('all', function (change, filepath) {
+    filepath = path.resolve(filepath);
+
+    var cheeseJSON = fs.readFileSync(path.join(dirname, 'cheese.json'),
+                                     { encoding: 'utf-8' });
+    var clientFiles = JSON.parse(cheeseJSON).client;
+    var mainFile = path.resolve(JSON.parse(cheeseJSON).main);
+
+    clientFiles = _.map(clientFiles, function (p) { return path.resolve(p); });
+
+    var listed = _.some(_.map(clientFiles, function (p) {
+      return _.contains(filepath, p);
+    }));
+
+    // only reload when changes are made to files listed in cheese.json
+    if (listed || mainFile === filepath) {
+      debug('reloading server');
+      startServer(dirname, port, true);
+    }
   });
 }
 
