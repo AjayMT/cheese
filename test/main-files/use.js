8e04cd65ceabc33be7435769def85206036a5fca
Ajay MT <ajay.tatachar@gmail.com>
2014-10-14 16:06:22 +0530

    use() only works with functions

--- test/main-files/use.js
+++
@@ -1,8 +0,0 @@
-
-/* global require, module */
-
-var Cheese = require('../../lib/main.js');
-
-Cheese.opt('foo', 'bar');
-
-module.exports = Cheese;
--- test/use.js
+++ test/use.js
@@ -9,16 +9,12 @@ describe('Cheese#use()', function () {
   var Cheese = require('../lib/main.js');
 
   it('should change the Cheese object', function () {
-    Cheese.use(function (Cheese) {
-      Cheese.opt('hello', 'world');
+    Cheese.use(function (C) {
+      C.opt('hello', 'world');
+
+      return C;
     });
 
     Cheese.opt('hello').should.equal('world');
-  });
-
-  it('should work with main files', function () {
-    Cheese.use(path.join(__dirname, 'main-files', 'use.js'));
-
-    Cheese.opt('foo').should.equal('bar');
   });
 });
